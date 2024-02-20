5dbec8e7b522aaaff018f36a30dcce1e0585d872
Brian Perry <contact.brianperry@gmail.com>
2024-02-20 08:18:56 -0600

    Make terminal interactive

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { WebContainer } from '@webcontainer/api';
-import { Terminal } from 'xterm'
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 
 import { files } from './files';
 
@@ -9,38 +10,29 @@ import './style.css';
 /** @type {import('@webcontainer/api').WebContainer}  */
 let webcontainerInstance;
 
-async function installDependencies(terminal) {
-  // Install dependencies
-  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
-
-  // It is possible to read the output of a command
-  installProcess.output.pipeTo(new WritableStream({
-    write(data) {
-      terminal.write(data);
-    }
-  }));
-
-  // Wait for install command to exit
-  return installProcess.exit;
-}
-
-async function startDevServer(terminal) {
-  // Run `npm run start` to start the Express app
-  const serverProcess = await webcontainerInstance.spawn('npm', ['run', 'start']);
-
-  serverProcess.output.pipeTo(
+/**
+ * @param {Terminal} terminal
+ */
+async function startShell(terminal) {
+  const shellProcess = await webcontainerInstance.spawn('jsh', {
+    terminal: {
+      cols: terminal.cols,
+      rows: terminal.rows,
+    },
+  });
+  shellProcess.output.pipeTo(
     new WritableStream({
       write(data) {
         terminal.write(data);
       },
     })
   );
-
-  // Wait for `server-ready` event
-  webcontainerInstance.on('server-ready', (port, url) => {
-    iframeEl.src = url;
+  const input = shellProcess.input.getWriter();
+  terminal.onData((data) => {
+    input.write(data);
   });
-}
+  return shellProcess;
+};
 
 /** @param {string} content*/
 async function writeIndexJS(content) {
@@ -49,26 +41,37 @@ async function writeIndexJS(content) {
 
 window.addEventListener('load', async () => {
   textareaEl.value = files['index.js'].file.contents;
-  
   textareaEl.addEventListener('input', (e) => {
     writeIndexJS(e.currentTarget.value);
   });
 
+  const fitAddon = new FitAddon();
+
   const terminal = new Terminal({
     convertEol: true,
   });
+  terminal.loadAddon(fitAddon);
   terminal.open(terminalEl);
+
+  fitAddon.fit();
 
   // Call only once
   webcontainerInstance = await WebContainer.boot();
   await webcontainerInstance.mount(files);
 
-  const exitCode = await installDependencies(terminal);
-  if (exitCode !== 0) {
-    throw new Error('Installation failed');
-  };
+  // Wait for `server-ready` event
+  webcontainerInstance.on('server-ready', (port, url) => {
+    iframeEl.src = url;
+  });
 
-  startDevServer(terminal);
+  const shellProcess = await startShell(terminal);
+  window.addEventListener('resize', () => {
+    fitAddon.fit();
+    shellProcess.resize({
+      cols: terminal.cols,
+      rows: terminal.rows,
+    });
+  });
 });
 
 document.querySelector('#app').innerHTML = `
